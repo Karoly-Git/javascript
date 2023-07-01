798a1c09c610d55ca58dfca6cdd9ffc7f5f1c035
Karoly <hornyal.mobile@gmail.com>
2023-07-01 07:44:16 +0100

    adding other solutions and testing performance

--- find_min_max.js
+++ find_min_max.js
@@ -4,33 +4,160 @@
 // The given array.
 let array = [12, 4, -5, 27, 39, 11, 63, 42, -9, 21, 17, 49];
 
-const findMinMax = (list) => {
-    // Let's declare 'min' and set 'min.value' and 'min.index' to 0.
-    let min = { value: 0, index: 0 };
 
-    // Let's iterate through the list and compare if any of the elements has greater value than 'min.value'. 
-    for (let i in list) {
-        // If yes, then overwrite the 'min.value' to the current value and the 'min.index' to the current index.
+// ********************
+// Solution-1:
+// for... loop
+const findMinMaxS1 = (list) => {
+    // Let's declare 'min' and 'max' objects, and set all their values to 0.
+    let min = { value: 0, index: 0 };
+    let max = { value: 0, index: 0 };
+
+    // Let's iterate through the list and compare if any of the elements has smaller value than 'min.value' or greater than 'max.value'. 
+    for (let i = 0; i < list.length; i++) {
+        // If any of the elements has smaller value than 'min.value', then overwrite the 'min.value' to the current value and the 'min.index' to the current index.
         if (list[i] < min.value) {
             min.value = list[i];
             min.index = i;
         }
-    }
-
-    // Let's declare 'max' and set 'max.value' and 'max.index' to 0.
-    let max = { value: 0, index: 0 };
-
-    // Let's iterate through the list and compare if any of the elements has smaller value than 'max.value'. 
-    for (let j in list) {
-        // If yes, then overwrite the 'max.value' to the current value and the 'max.index' to the current index.
-        if (list[j] > max.value) {
-            max.value = list[j];
-            max.index = j;
+        // If any of the elements has greater value than 'max.value', then overwrite the 'max.value' to the current value and the 'max.index' to the current index.
+        if (list[i] > max.value) {
+            max.value = list[i];
+            max.index = i;
         }
     }
     // Return the object that has all required data.
     return { min, max };
 }
 
-// Log the result.
-console.log(findMinMax(array));
+// Log the results.
+console.log('S1, for...\n', findMinMaxS1(array));
+
+
+// ********************
+// Solution-2:
+// for...in loop
+const findMinMaxS2 = (list) => {
+    /*
+    When you use for...in loop, it iterates over the indices of the array as strings,
+    not as numerical indices. Therefore, the i variable represents the index of the array as a string, not a number.
+    If there is a need to store it as a number, then it has to be converted to a number.
+    */
+    let min = { value: 0, index: 0 };
+    let max = { value: 0, index: 0 };
+
+    for (let i in list) {
+        if (list[i] < min.value) {
+            min.value = list[i];
+            min.index = Number(i);
+        }
+        if (list[i] > max.value) {
+            max.value = list[i];
+            max.index = Number(i);
+        }
+    }
+    return { min, max };
+}
+
+console.log('S2, for...in\n', findMinMaxS2(array));
+
+
+// ********************
+// Solution-3
+// forEach()
+const findMinMaxS3 = (list) => {
+    let min = { value: 0, index: 0 };
+    let max = { value: 0, index: 0 };
+
+    list.forEach((element, index) => {
+        if (element < min.value) {
+            min.value = element;
+            min.index = index;
+        }
+        if (element > max.value) {
+            max.value = element;
+            max.index = index;
+        }
+    });
+    return { min, max };
+}
+
+console.log('S3, forEach()\n', findMinMaxS3(array));
+
+
+/* !!!   TO BE ADDED FROM HERE   !!! */
+
+// Solution-4
+// forEach() and ternary
+const findMinMaxS4 = (list) => {
+    let min = { value: 0, index: 0 };
+    let max = { value: 0, index: 0 };
+
+    list.forEach((element, index) => {
+        element < min.value ? ((min.value = element), (min.index = index)) : false;
+        element > max.value ? ((max.value = element), (max.index = index)) : false;
+    });
+    return { min, max };
+}
+
+console.log('forEach & ternary\n', findMinMaxS4(array));
+
+// Solution-5
+// built in methods
+const findMinMaxS5 = (list) => {
+    let min = { value: 0, index: 0 };
+    let max = { value: 0, index: 0 };
+    min.value = Math.min(...array);
+    min.index = array.indexOf(Math.min(...array));
+    max.value = Math.max(...array);
+    max.index = array.indexOf(Math.max(...array));
+    return { min, max };
+}
+
+console.log('built in functions\n', findMinMaxS5(array));
+
+(function test() {
+    let start;
+    let end;
+
+    start = performance.now();
+    findMinMaxS1(array);
+    end = performance.now();
+    console.log(end - start);
+
+    start = performance.now();
+    findMinMaxS2(array);
+    end = performance.now();
+    console.log(end - start);
+
+    start = performance.now();
+    findMinMaxS3(array);
+    end = performance.now();
+    console.log(end - start);
+
+    start = performance.now();
+    findMinMaxS4(array);
+    end = performance.now();
+    console.log(end - start);
+
+    start = performance.now();
+    findMinMaxS5(array);
+    end = performance.now();
+    console.log(end - start);
+
+})();
+
+const arraySize = 100000;
+const min = -100000;
+const max = 100000;
+
+const getRandomInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const arrayNew = Array.from({ length: arraySize }, () =>
+    getRandomInteger(min, max)
+);
+
+
+//console.log(array);
--- find_missing_integer.js
+++ find_missing_integer.js
@@ -1,6 +1,6 @@
-// Given an array of integers.
-// The array is sorted, the first element is 1, the increment is 1.
-// 1 element is missing.
+// Given a sorted array of integers.
+// The integers are from 1 to n, increment is 1.
+// 1 element is missing (but not the n-th element).
 // Task: Find the missing element.
 
 // This returns a GOOD array with NO missing element
@@ -27,6 +27,7 @@ function makeBadArray(numOfElements = 10, makeItMissing = 5) {
 }
 
 // Solutin-1:
+// For Loop
 function findMissingElementS1(array) {
     if (array.length < 2) {
         return 'Invalid input. The array should have at least two elements.';
@@ -44,19 +45,109 @@ function findMissingElementS1(array) {
     return 'No missing number.'
 }
 
+// Solution-2:
+// Binary Search
+function findMissingElementS2(arrayGood, arrayBad) {
+    //
+    let startIndex = 0;
+    let endIndex = arrayGood.length - 1;
+    let middleIndex = startIndex + Math.round((endIndex - startIndex) / 2 - 0.5);
+    let isMatching;
+    //
+    while (endIndex - startIndex > 2) {
+        isMatching = arrayBad[middleIndex] === arrayGood[middleIndex];
+        //
+        if (isMatching) {
+            startIndex = middleIndex;
+            middleIndex = startIndex + Math.round((endIndex - startIndex) / 2 - 0.5);
+        }
+        //
+        if (!isMatching) {
+            endIndex = middleIndex;
+            middleIndex = startIndex + Math.round((endIndex - startIndex) / 2 - 0.5);
+        }
+    }
+    //
+    for (let i = startIndex; i <= endIndex; i++) {
+        //
+        if (arrayBad[i] !== arrayGood[i]) {
+            //
+            return arrayGood[i];
+        }
+    }
+    //
+    return 'No missing number.'
+}
+
 let numOfElements = 10000000;   // 10,000,000
-let makeItMissing = 9542001;    //  9,542,001
+let makeItMissing = 9542003;    //  9,542,003
 let goodList = makeGoodArray(numOfElements);
 let badList = makeBadArray(numOfElements, makeItMissing);
 
 function test1() {
-    let time1a = new Date().getTime();
+    let timeStart = performance.now();
     let missingNumber = findMissingElementS1(badList);
-    let time1b = new Date().getTime();
-    console.log(`\nSolution:\t1\nMissing number:\t${missingNumber}\nRun time:\t${time1b - time1a} ms`);
+    let timeEnd = performance.now();
+    console.log(`\nSolution:\t1\nMissing number:\t${missingNumber}\nRun time:\t${timeEnd - timeStart} ms`);
+    console.log('----------');
+}
+
+function test2() {
+    let timeStart = performance.now();
+    let missingNumber = findMissingElementS2(goodList, badList);
+    let timeEnd = performance.now();
+    console.log(`\nSolution:\t2\nMissing number:\t${missingNumber}\nRun time:\t${timeEnd - timeStart} ms`);
     console.log('----------');
 }
 
 test1();
+test2();
 
-// Solution-2: coming up shortly...
+
+/******************/
+// adding descriotion
+
+// This time the array is unsorted.
+// Task: Find the missing element.
+
+function findMissingInUnsorted(array) {
+    let n = array.length + 1;               // +1, because that would be the number of elements if one of them wasn't missing
+    let calculatedSum = (n * (n + 1)) / 2;  // formula to calculate the sum of the first n natural numbers starting with 1
+    let countedSum = 0;                     // sum for the loop
+    for (let i of array) {
+        countedSum += i;
+    }
+    // the missing number will be the differenc of the 'calculatedSum' and the 'countedSum'
+    return calculatedSum - countedSum;
+}
+
+let unsortedArray = [4, 7, 9, 1, 5, 3, 10, 2, 8];
+
+console.log(findMissingInUnsorted(unsortedArray));
+
+// This time the array is unsorted and there are multiple missing elements.
+// Task: Find all the missing elements.
+
+let multipleMissing = [4, 19, 11, 18, 16, 13, 17, 15, 1, 12, 20, 5, 10, 2, 14, 8]; // 3, 6, 7, 9
+
+function findMultipleMissing(array) {
+    let sortedArray = array.sort((a, b) => a - b);
+    let missingNumbers = [];
+    for (let i = 0; i < array.length - 2; i++) {
+        let currentNumber = sortedArray[i];
+        let nextNumber = sortedArray[i + 1];
+        if (nextNumber - currentNumber !== 1) {
+            for (let j = 1; j < (nextNumber - currentNumber); j++) {
+                missingNumbers.push(currentNumber + j);
+            }
+        }
+    }
+    return {
+        first: sortedArray[0],
+        missingNumbers, last:
+            sortedArray[sortedArray.length - 1]
+    };
+}
+
+console.log(findMultipleMissing(multipleMissing));
+
