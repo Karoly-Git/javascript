941d7044f7861defe3df7ad01b170d891006d9c1
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2023-07-02 08:27:45 +0100

    Adding Solution-4 to find_min_max.js

--- find_min_max.js
+++ find_min_max.js
@@ -2,46 +2,39 @@
 // Numbers: -14, 10, 7, -6, -16, 18, -19, 15, -1, -9, 12, -8, 4, 2, -3, 9, -17, 5, 0, -20
 // Task: Find the smallest and the greatest values of the array and return their values and positions.
 
-// The given array.
-let array = [-14, 10, 7, -6, -16, 18, -19, 15, -1, -9, 12, -8, 4, 2, -3, 9, -17, 5, 0, -20];
+const array = [-14, 10, 7, -6, -16, 18, -19, 15, -1, -9, 12, -8, 4, 2, -3, 9, -17, 5, 0, -20];
 
 
-// ********************
 // Solution-1:
 // for... loop
 const findMinMaxS1 = (list) => {
     // Declare 'min' and 'max' objects.
-    // Set both, the smallest and the greatest values to the value of the first element to.
-    // Set index 0. 
     let min = { value: list[0], index: 0 };
     let max = { value: list[0], index: 0 };
 
     // Iterate through the list and compare if any of the elements has smaller value than 'min.value' or greater than 'max.value'. 
     for (let i = 0; i < list.length; i++) {
-        // If any of the elements has smaller value than 'min.value', then overwrite the 'min.value' to the current value and the 'min.index' to the current index.
+        // If any there is a smaller value, then overwrite the value and the index.
         if (list[i] < min.value) {
             min.value = list[i];
             min.index = i;
         }
-        // If any of the elements has greater value than 'max.value', then overwrite the 'max.value' to the current value and the 'max.index' to the current index.
+        // If any there is a greater value, then overwrite the value and the index.
         if (list[i] > max.value) {
             max.value = list[i];
             max.index = i;
         }
     }
-    // Return the object that has all required data.
     return { min, max };
 }
 
-// Log the results.
 console.log('S1, for...\n', findMinMaxS1(array));
 
 
-// ********************
 // Solution-2:
 // for...in loop
 const findMinMaxS2 = (list) => {
-    /*
+    /* Note: 
     When you use for...in loop, it iterates over the indices of the array as strings,
     not as numerical indices. Therefore, the i variable represents the index of the array as a string, not a number.
     If there is a need to store it as a number, then it has to be converted to a number.
@@ -65,9 +58,8 @@ const findMinMaxS2 = (list) => {
 console.log('S2, for...in\n', findMinMaxS2(array));
 
 
-// ********************
 // Solution-3
-// forEach()
+// forEach loop
 const findMinMaxS3 = (list) => {
     let min = { value: list[0], index: 0 };
     let max = { value: list[0], index: 0 };
@@ -86,3 +78,19 @@ const findMinMaxS3 = (list) => {
 }
 
 console.log('S3, forEach()\n', findMinMaxS3(array));
+
+
+// Solution-4
+// forEach and ternary
+const findMinMaxS4 = (list) => {
+    let min = { value: list[0], index: 0 };
+    let max = { value: list[0], index: 0 };
+
+    list.forEach((element, index) => {
+        element < min.value ? ((min.value = element), (min.index = index)) : false;
+        element > max.value ? ((max.value = element), (max.index = index)) : false;
+    });
+    return { min, max };
+}
+
+console.log('forEach & ternary\n', findMinMaxS4(array));
