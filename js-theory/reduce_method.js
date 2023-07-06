f717fdbe71d6ef35d9914bc9ae86bc88980eb67e
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2023-07-06 05:15:54 +0100

    Adding description to reduce_method.js

--- js-theory/reduce_method.js
+++ js-theory/reduce_method.js
@@ -1,3 +1,8 @@
+/* In JavaScript, the reduce() method is used to iterate over an 
+array and accumulate the values into a single result. It applies 
+a callback function to each element of the array and returns a single value.
+*/
+
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let sum = arr.reduce((a, b) => a + b, 100);
