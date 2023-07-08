b2168c8d2721be8fa8cebccb5f11cd65dccfee70
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2023-07-08 05:57:35 +0100

    Update config.js

---
+++ blocks/img_carousel/config.js
@@ -0,0 +1,7 @@
+const config = {
+    numOfSlides: 5,     //Number of files are in the 'img' folder.
+    slidingTime: 1500,  //Carousel sliding time in milliseconds.
+    timeGap: 4000       //Time gap (waiting time) between slides in milliseconds.
+}
+
+export default config;
