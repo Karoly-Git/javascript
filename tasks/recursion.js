fefb23f49e059f1ea36bce27c2e5defe6a84d36c
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2023-08-02 05:23:46 +0100

    Update recursion.js

--- tasks/recursion.js
+++ tasks/recursion.js
@@ -7,3 +7,15 @@ function factorial(n) {
     }
 }
 
+
+// Calculate the sum of the first n natural numbers
+function sum(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return n + sum(n - 1);
+    }
+}
+
