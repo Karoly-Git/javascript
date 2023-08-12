d3eb21a61be96addd3378650a749624cb40418e4
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2023-08-12 09:11:07 +0100

    Update promise.js

--- js-theory/promise.js
+++ js-theory/promise.js
@@ -1,1 +1,20 @@
+let p = new Promise((resolve, reject) => {
+    let num = null;
+    setTimeout(() => {
+        num = 2;
+        if (num === 2) {
+            resolve('Hey, happy days! Promise fulfilled.');
+        } else {
+            reject('Ooops, something went wrong! Promise rejected.');
+        }
+    }, 1000);
+});
+
+p.then(message => {
+    console.log(message);
+}).catch(message => {
+    console.log(message);
+});
+
+console.log('Should be last, but wont be.');
 
